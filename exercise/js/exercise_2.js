f1d9dcd355f0bf384458eece401bb0ee4e54a93e
Ghileors <58727406+Ghileors@users.noreply.github.com>
2020-05-19 13:11:30 +0300

    Completed task 2.

--- exercise/js/exercise_2.js
+++ exercise/js/exercise_2.js
@@ -1,25 +1,32 @@
-const account = {
-    owner: 'Mango',
-    balance: 24000,
-    discount: 0.1,
-    orders: ['order-1', 'order-2', 'order-3'],
-    changeDiscount(value) {
-        this.discount = value;
+const inventory = {
+    items: ['Knife', 'Gas mask'],
+
+    add(itemName) {
+        console.log(`Adding ${itemName} to inventory`);
+
+        this.items.push(itemName);
     },
-    showOrders() {
-        return this.orders;
-    },
-    addOrder(cost, order) {
-        this.balance -= cost;
-        this.orders.push(order);
+
+    remove(itemName) {
+        console.log(`Removing ${itemName} from inventory`);
+
+        this.items = this.items.filter(item => item !== itemName);
     },
 };
 
-account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+const invokeInventoryAction = function (itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action(itemName);
+};
 
-console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+// Invoking action on Medkit
+// Adding Medkit to inventory
 
-account.addOrder(5000, 'order-4');
-console.log(account.balance); // 19000
-console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
